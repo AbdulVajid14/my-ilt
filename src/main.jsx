f687430b3e59d2000e6065ca1fd1028eb52eed58
AbdulVajidK <abdulvajid051@gmail.com>
2026-06-18 11:23:58 +0530

    Remove .env from git tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  // react-snap prerendered this page → hydrate existing HTML
+  hydrateRoot(
+    rootElement,
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+} else {
+  // Normal client render (dev mode or non-prerendered route)
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+}
